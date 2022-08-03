e9847e177c6b53a235da81c199e58f461a0174a7
Matias Cionci <cioncimatias2@gmail.com>
2022-08-03 02:18:59 -0300

    Verificando commit

--- app/src/main/java/com/example/pruebagit/MainActivity.kt
+++ app/src/main/java/com/example/pruebagit/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.pruebagit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pruebagit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-                //nuevo cambio
+
     }
 }
